9b4ed4b5605ca9b34984c73b9a98518ab47f67e2
jasond-qa <jason.derry@academytrainee.com>
2019-01-15 12:35:44 +0000

    added account class

---
+++ src/main/java/Account.java
@@ -0,0 +1,35 @@
+
+public class Account {
+	
+	private String firstName;
+	private String lastName;
+	private int accountNumber;
+	
+	Account (String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	
+	
+
+}
