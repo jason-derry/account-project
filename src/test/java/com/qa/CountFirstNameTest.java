f3534d2ba8780cd1d48b9a8ab4f6e27e9fa35df5
jasond-qa <jason.derry@academytrainee.com>
2019-01-16 10:34:44 +0000

    separated tests

---
+++ src/test/java/com/qa/CountFirstNameTest.java
@@ -0,0 +1,35 @@
+package com.qa;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CountFirstNameTest {
+	
+	AccountService ac1 = new AccountService();
+	
+	@Before
+	public void addNames() {
+		ac1.addAccount("Jeff", "Bridges");
+		ac1.addAccount("Jeremy", "Kyle");
+		ac1.addAccount("Jeff", "Dunham");
+		ac1.addAccount("Jeremy", "Clarkson");
+		ac1.addAccount("Jeff", "Bezos");
+		ac1.addAccount("Michael", "Scarn");
+		ac1.addAccount("Jeff", "Goldblum");
+	}
+	@Test public void testCountFirstName() {
+		assertEquals(4, ac1.countFirstName("jeff"));
+	}
+	
+	@Test public void testCountFirstName2() {
+		assertEquals(2, ac1.countFirstName("jeremy"));
+	}
+	
+	@Test public void testCountFirstName3() {
+		assertEquals(1, ac1.countFirstName("michael"));
+	}
+	
+
+}
