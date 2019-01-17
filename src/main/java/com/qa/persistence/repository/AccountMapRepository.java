450040f6557a2992c6f1ea6a3e20bea7c02485f3
jasond-qa <jason.derry@academytrainee.com>
2019-01-17 12:14:14 +0000

    made AccountMapRepository alternative cdi implementation

--- src/main/java/com/qa/persistence/repository/AccountMapRepository.java
+++ src/main/java/com/qa/persistence/repository/AccountMapRepository.java
@@ -5,26 +5,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.enterprise.inject.Alternative;
 import javax.inject.Inject;
 import javax.persistence.Query;
 
 import com.qa.persistence.domain.Account;
 import com.qa.util.JSONUtil;
 
+@Alternative
 public class AccountMapRepository implements AccountRepository {
 	
 	HashMap<Integer, Account> account = new HashMap<>();
 	int counter = 1;
 	
+	@Inject
+	private JSONUtil util;
+
 	@Override
 	public String getAllAccounts() {
 		String result = account.values().stream().map(Object::toString).collect(Collectors.joining(", "));
 		return result;
 	}
 	
-	@Inject
-	private JSONUtil util;
-
 	@Override
 	public String createAccount(String accountToAdd) {
 		Account anAccount = util.getObjectForJSON(accountToAdd, Account.class);
